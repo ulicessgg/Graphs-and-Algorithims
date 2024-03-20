e3fc737ed65e1d7ee74764fcebf1912fda3807ad
Odera Nwosu <oderanwosu.j@gmail.com>
2024-03-20 12:51:18 -0700

    refactor(codebase): update file names and add design documentation for quickSort algorithm

---
+++ designDoc.h
@@ -0,0 +1,17 @@
+/*
+
+Input and output of the problem
+quickSort() ->
+    ({TokenFreq[freq: 1], TokenFreq[freq: 3], TokenFreq[freq: 2]}, sortAsc | {TokenFreq[freq: 1], TokenFreq[freq: 2], TokenFreq[freq: 3]})
+    ({TokenFreq[freq: 1], TokenFreq[freq: 3], TokenFreq[freq: 2]}, sortDesc | {TokenFreq[freq: 3], TokenFreq[freq: 2], TokenFreq[freq: 1]})
+your algorithm in pseudo code with comments
+quickSort() ->
+    Algorithm:
+        // Partition the full array into two halves 
+    
+worst-case time complexity analysis
+    O(nlogn)
+your choice of data structures with a brief justification in the final coding stage
+
+
+*/
