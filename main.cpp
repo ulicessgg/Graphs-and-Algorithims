1b8b2208271bc31a01f38fe011023aadd9b1e11b
Odera Nwosu <oderanwosu.j@gmail.com>
2024-03-20 12:58:22 -0700

    refactor(build): remove unused uniform\_distr module, rename normDist.cpp to norm\_distr.cpp for consistency

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "project.h"
 #include "generic_quick_sort.cpp"
-#include "normDist.cpp"
+#include "norm_distr.cpp"
 #include "unitTests.h"
 
-int main(){
+int main()
+{
     UNIT_TESTS::unitTest1_quickSort();
     UNIT_TESTS::unitTest2_quickSort();
     UNIT_TESTS::unitTest3_quickSort();
