e262fedb4c86bc97a3dff49a545662522bb32e57
fatma-al <142875765+fatma-al@users.noreply.github.com>
2024-03-19 19:19:58 -0700

    uniform distrib

--- uniform_distr.cpp
+++ uniform_distr.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 #include <random>
+#include <string>
+#include <iomanip>
 
+
+
+void printingHistogram( std::vector<double> dataNums ){
+    char symbol = '*';
+    if (dataNums.empty()){
+        std:: cout << "Empty";
+    }
+    for ( int i = 0; i < dataNums.size(); i++ ){
+        std::cout << i << " - " << i+1.0 << "|" ;
+        std::cout << std::string(dataNums[i]*200.0/20000.0,'*') << std::endl;
+    }
+
+}
 void createHistogram(double rangeMin, double rangeMax){
     const int dataSize = 20000;
+    const int intervals = 10;
+    const int numStars = 95;
     std::uniform_real_distribution<double> distribution(rangeMin, rangeMax);
+    std:: vector<double> dataNums (rangeMax + 1, 0);
+    if ( rangeMax - rangeMin == 0) {
+        std:: cout << "Range is equal to 0. Distribution not possible" << std:: endl;
+        return;
+    }
+    std::default_random_engine randomNums;
+    double tempNums;
+    for ( int i = 0; i < dataSize; i++) {
+        tempNums = distribution(randomNums);
+        dataNums[int(tempNums * intervals)]++;
+    }
+    printingHistogram(dataNums);
+
 }
-
-void printingHistogram(){
-
-}
-
-
 int main() {
     double rangeMin;
     double rangeMax;
@@ -18,10 +42,38 @@ int main() {
     std:: cin >> rangeMin;
     std:: cout << "What is your maximum number? ";
     std:: cin >> rangeMax;
-
+    createHistogram( rangeMin, rangeMax);
     return 0;
 }
 
+//mynums holding frequency of the values. temp holding actual values
+
+
+
+//
+//
+//const int size=20000;  // number of experiments
+//const int nstars=95;     // maximum number of stars to distribute
+//const int nintervals=10; // number of intervals so for example 0 to 1, 1 to 2, 2 to 3 etc
+//
+//std::default_random_engine seed;
+//std::uniform_real_distribution<double> distribution(lowerBound,upperBound);
+//
+//int p[nintervals]={};
+//
+//for (int i=0; i<size; ++i) {
+//double number = distribution(generator);
+//++p[int(nintervalsnumber)];
+//}
+//
+//std::cout << std::fixed; std::cout.precision(1);
+//
+//for (int i=0; i<nintervals; ++i) {
+//std::cout << float(i)/nintervals << "-" << float(i+1)/nintervals << ": ";
+//std::cout << std::string(p[i]nstars/nrolls,'*') << std::endl;
+//}
+//
+//
 
 
 
