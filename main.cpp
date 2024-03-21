49e8d73f8619c37733b4b1a25aa97f92da76c86d
Ulices Gonzalez <137358277+ulicessgg@users.noreply.github.com>
2024-03-20 19:20:49 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,36 +12,49 @@ int main()
     UNIT_TESTS::unitTest2_partition();
 
      if (!UNIT_TESTS::unitTest1_genRandNormalDist())
-     std::cout << "unitTest1_genRandNormalDist failed!\n";
-     if (!UNIT_TESTS::unitTest2_genRandUniformDist())
-         std::cout << "unitTest2_genRandUniformDist failed!\n";
-     if (!UNIT_TESTS::unitTest3_genRandUniformDist())
-         std::cout << "unitTest3_genRandUniformDist failed!\n";
-     if (!UNIT_TESTS::unitTest1_calcNormalHistStats())
-         std::cout << "unitTest1_calcNormalHistStats failed!\n";
-     if (!UNIT_TESTS::unitTest2_calcNormalHistStats())
-         std::cout << "unitTest2_calcNormalHistStats failed!\n";
-     if (!UNIT_TESTS::unitTest1_printNormalHist())
-         std::cout << "unitTest1_printNormalHist failed!\n";
-     if (!UNIT_TESTS::unitTest2_printNormalHist())
-         std::cout << "unitTest2_printNormalHist failed!\n";
-     
-     std::vector<int> userNums;
-     int userMean;
-     int userSD;
-     char userSym;
+    {
+        std::cout << "unitTest1_genRandNormalDist failed!" << std::endl;
+    }
+    if (!UNIT_TESTS::unitTest2_genRandUniformDist())
+    {
+        std::cout << "unitTest2_genRandUniformDist failed!" << std::endl;
+    }
+    if (!UNIT_TESTS::unitTest3_genRandUniformDist())
+    {
+        std::cout << "unitTest3_genRandUniformDist failed!" << std::endl;
+    }
+    if (!UNIT_TESTS::unitTest1_calcNormalHistStats())
+    { 
+        std::cout << "unitTest1_calcNormalHistStats failed!" << std::endl;
+    }
+    if (!UNIT_TESTS::unitTest2_calcNormalHistStats())
+    { 
+        std::cout << "unitTest2_calcNormalHistStats failed!" << std::endl;
+    }
+    if (!UNIT_TESTS::unitTest1_printNormalHist())
+    { 
+        std::cout << "unitTest1_printNormalHist failed!" << std::endl;
+    }
+    if (!UNIT_TESTS::unitTest2_printNormalHist())
+    {
+        std::cout << "unitTest2_printNormalHist failed!" << std::endl;
+    }
     
-     std::cout << "Enter mean: ";
-     std::cin >> userMean;
-     std::cout << "Enter standard deviation: ";
-     std::cin >> userSD;
-     std::cout << std::endl;
+    std::vector<int> userNums;
+    int userMean;
+    int userSD;
+    char userSym;
     
-     NS_TEAM_PROJECT::genRandNormalDist(userNums, userMean, userSD);
-     NS_TEAM_PROJECT::calcNormalHistStats(userNums, userMean, userSD);
-     NS_TEAM_PROJECT::printNormalHist(userNums, userSym, userMean);
-
-
+    std::cout << "Enter mean: ";
+    std::cin >> userMean;
+    std::cout << "Enter standard deviation: ";
+    std::cin >> userSD;
+    std::cout << std::endl;
+    
+    NS_TEAM_PROJECT::genRandNormalDist(userNums, userMean, userSD);
+    NS_TEAM_PROJECT::calcNormalHistStats(userNums, userMean, userSD);
+    NS_TEAM_PROJECT::printNormalHist(userNums, userSym, userMean);
+    
     return 0;
 }
 
