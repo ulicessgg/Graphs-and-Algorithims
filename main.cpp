5591de1e9b5724d1e621458cdb499d24a203c226
Odera Nwosu <oderanwosu.j@gmail.com>
2024-03-20 19:06:34 -0700

    refactor(unitTests): add new unit tests for `genRandNormalDist`, `calcNormalHistStats`, and `printNormalHist` functions, and prompt user to input mean and standard deviation for test data generation.

--- main.cpp
+++ main.cpp
@@ -8,11 +8,38 @@ int main()
     UNIT_TESTS::unitTest1_quickSort();
     UNIT_TESTS::unitTest2_quickSort();
     UNIT_TESTS::unitTest3_quickSort();
-    UNIT_TESTS::unitTest1_genRandNormalDist();
-    UNIT_TESTS::unitTest2_genRandNormalDist();
-    UNIT_TESTS::unitTest3_genRandNormalDist();
     UNIT_TESTS::unitTest1_partition();
     UNIT_TESTS::unitTest2_partition();
 
+    if (!UNIT_TESTS::unitTest1_genRandNormalDist())
+        std::cout << "unitTest1_genRandNormalDist failed!\n";
+    if (!UNIT_TESTS::unitTest2_genRandUniformDist())
+        std::cout << "unitTest2_genRandUniformDist failed!\n";
+    if (!UNIT_TESTS::unitTest3_genRandUniformDist())
+        std::cout << "unitTest3_genRandUniformDist failed!\n";
+    if (!UNIT_TESTS::unitTest1_calcNormalHistStats())
+        std::cout << "unitTest1_calcNormalHistStats failed!\n";
+    if (!UNIT_TESTS::unitTest2_calcNormalHistStats())
+        std::cout << "unitTest2_calcNormalHistStats failed!\n";
+    if (!UNIT_TESTS::unitTest1_printNormalHist())
+        std::cout << "unitTest1_printNormalHist failed!\n";
+    if (!UNIT_TESTS::unitTest2_printNormalHist())
+        std::cout << "unitTest2_printNormalHist failed!\n";
+
+    std::vector<int> userNums;
+    int userMean;
+    int userSD;
+
+    std::cout << "Enter mean: ";
+    std::cin >> userMean;
+    std::cout << "Enter standard deviation: ";
+    std::cin >> userSD;
+    std::cout << std::endl;
+
+    NS_TEAM_PROJECT::genRandNormalDist(userNums, userMean, userSD);
+
     return 0;
 }
+
+
+
