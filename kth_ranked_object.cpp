794927fcfa985ae9de9c0d3b3c500c17e492e194
John Collins <jcollins9@sfsu.edu>
2024-03-20 23:26:04 -0700

    Added helpful comments to kth_ranked_object

--- kth_ranked_object.cpp
+++ kth_ranked_object.cpp
@@ -3,18 +3,26 @@
 #include <cstdlib>
 #include "project.h"
 
+// function to generate the kth ranked object
 NS_TEAM_PROJECT::TokenFreq genKthRanked(std::vector<NS_TEAM_PROJECT::TokenFreq> &arr, int k, std::function<bool(NS_TEAM_PROJECT::TokenFreq, NS_TEAM_PROJECT::TokenFreq)> comp) {
     int left = 0;
     int right = arr.size() -1;
-
+    
+    // Loop until the kth ranked object is found
     while(true) {
+	// select a random pivot
         int pivot = std::rand() % (right - left + 1) + left;
-        int new_pivot = NS_TEAM_PROJECT::partition(arr, left, right, comp);
-        if(new_pivot == arr.size() - k) {
+        // partition array around the pivot
+	int new_pivot = NS_TEAM_PROJECT::partition(arr, left, right, comp);
+        
+	// if the pivot is the kth ranked object, return it
+	if(new_pivot == arr.size() - k) {
             return arr[new_pivot];
+	// search the left side
         } else if (new_pivot > arr.size() - k) {
             right = new_pivot - 1;
-        } else {
+        // search the right side
+	} else {
             left = new_pivot + 1;
         }
     }
