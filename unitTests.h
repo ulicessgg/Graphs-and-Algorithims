d9b759e0364a660500113fa9b73a6b13145091fa
Ulices Gonzalez <137358277+ulicessgg@users.noreply.github.com>
2024-03-20 19:17:00 -0700

    Update unitTests.h

--- unitTests.h
+++ unitTests.h
@@ -14,26 +14,26 @@ namespace UNIT_TESTS
 	{
 		std::vector<int> userNums;
 		NS_TEAM_PROJECT::genRandNormalDist(userNums, 0, 1); // with a mean of zero there can't be a normal distribution generated
-		return userNums.empty();							// returns true if userNums is empty
+		return userNums.empty(); // returns true if userNums is empty
 	}
-
+	
 	bool unitTest2_genRandUniformDist()
 	{
 		std::vector<int> userNums;
 		NS_TEAM_PROJECT::genRandNormalDist(userNums, 1, 0); // with a stdDev of zero there can't be a normal distribution generated
-		return userNums.empty();							// returns true if userNums is empty
+		return userNums.empty(); // returns true if userNums is empty
 	}
-
+	
 	bool unitTest3_genRandUniformDist()
 	{
 		std::vector<int> userNums;
 		NS_TEAM_PROJECT::genRandNormalDist(userNums, 9, 3);
 		return userNums.size() == 15; // returns true if userNums has mean + 6 as its size
 	}
-
+	
 	bool unitTest1_calcNormalHistStats()
 	{
-		std::vector<int> userNums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+		std::vector<int> userNums = { 1,2,3,4,5,6,7,8,9,10,11,10,9,8,7,6,5,4,3,2,1 };
 		double mean = 10;
 		double stdDev = 15;
 		double median;
@@ -53,7 +53,7 @@ namespace UNIT_TESTS
 	}
 	bool unitTest2_calcNormalHistStats()
 	{
-		std::vector<int> userNums = {1, 2, 4, 8, 4, 2, 1};
+		std::vector<int> userNums = { 1,2,4,8,4,2,1 };
 		double mean = 3;
 		double stdDev = 1;
 		double median;
@@ -71,26 +71,28 @@ namespace UNIT_TESTS
 			return false;
 		}
 	}
-
+	
 	bool unitTest1_printNormalHist()
 	{
 		std::vector<int> userNums;
 		double mean = 0;
 		char sym = '*';
-
+	
 		NS_TEAM_PROJECT::printNormalHist(userNums, sym, mean);
-
+	
 		return (sym == '*');
 	}
-
+	
 	bool unitTest2_printNormalHist()
 	{
-		std::vector<int> userNums = {1, 2, 4, 8, 4, 2, 1};
+		std::vector<int> userNums;
 		double mean = 8;
+		double stdDev = 2;
 		char sym = '*';
-
+	
+		NS_TEAM_PROJECT::genRandNormalDist(userNums, mean, stdDev);
 		NS_TEAM_PROJECT::printNormalHist(userNums, sym, mean);
-
+	
 		return !(sym == '*');
 	}
 
